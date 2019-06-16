b8e19192526b545cd122b004a8f4d9230b30b9a0
web-yk5160 <rubystudy0123@gmail.com>
2019-06-16 14:24:47 +0900

    introduced request objects

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
                                     <a href="">Post</a>
                                 </li>
                                 <li class="list-group-item">
-                                    <a href="">Categories</a>
+                                    <a href="{{ route('categories.index') }}">Categories</a>
                                 </li>
                             </ul>
                         </div>

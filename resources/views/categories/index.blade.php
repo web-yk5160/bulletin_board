3cb247c7fea8c48b78b4af883415e81259813ac8
web-yk5160 <rubystudy0123@gmail.com>
2019-06-16 15:49:54 +0900

    Added updating categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -18,6 +18,11 @@
             <td>
               {{ $category->name }}
             </td>
+            <td>
+              <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">
+                Edit
+              </a>
+            </td>
           </tr>
         @endforeach
       </tbody>

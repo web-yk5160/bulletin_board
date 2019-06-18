1d1bca890194b770a26897fda021b343d02af7ae
web-yk5160 <rubystudy0123@gmail.com>
2019-06-18 10:40:44 +0900

    post associated to handle delete tag and category

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -95,6 +95,13 @@ class TagsController extends Controller
      */
     public function destroy(Tag $tag)
     {
+
+        if ($tag->posts->count() > 0) {
+            session()->flash('error', 'Tag cannot be deleted because it associated some posts.');
+
+            return redirect()->back();
+        }
+
         $tag->delete();
 
         session()->flash('success', 'Tag deleted successfully.');

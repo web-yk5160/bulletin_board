d3de2139cf7df04c81373e86ebded468e3c92c0d
web-yk5160 <rubystudy0123@gmail.com>
2019-06-19 11:10:13 +0900

    Created top page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,15 +27,15 @@
         <div class="navbar-left">
           <button class="navbar-toggler" type="button">&#9776;</button>
           <a class="navbar-brand" href="../index.html">
-            <img class="logo-dark" src="../assets/img/logo-dark.png" alt="logo">
-            <img class="logo-light" src="../assets/img/logo-light.png" alt="logo">
+            <img class="logo-dark" src="{{ asset('img/logo-dark.png') }}" alt="logo">
+            <img class="logo-light" src="{{ asset('img/logo-light.png') }}" alt="logo">
           </a>
         </div>
 
         <section class="navbar-mobile">
           <span class="navbar-divider d-mobile-none"></span>
 
-          <ul class="nav nav-navbar">
+          <!-- <ul class="nav nav-navbar">
             <li class="nav-item">
               <a class="nav-link" href="#">Demos <span class="arrow"></span></a>
               <ul class="nav">
@@ -188,9 +188,9 @@
                 <a class="nav-link" href="../shop/cart.html">Cart</a>
                 <a class="nav-link" href="../shop/checkout.html">Checkout</a>
               </nav>
-            </li>
+            </li> -->
 
-            <li class="nav-item nav-mega">
+            <!-- <li class="nav-item nav-mega">
               <a class="nav-link" href="#">Blocks <span class="arrow"></span></a>
               <nav class="nav px-lg-2 py-lg-4">
                 <div class="container-fluid">
@@ -315,12 +315,12 @@
                   </div>
                 </div>
               </nav>
-            </li>
+            </li> -->
 
           </ul>
         </section>
 
-        <a class="btn btn-xs btn-round btn-success" href="https://themeforest.net/item/thesaas-responsive-bootstrap-saas-software-webapp-template/19778599?license=regular&open_purchase_for_item_id=19778599&purchasable=source&ref=thethemeio">Buy Now</a>
+        <a class="btn btn-xs btn-round btn-success" href="{{ route('login') }}">Login</a>
 
       </div>
     </nav><!-- /.navbar -->
@@ -352,72 +352,21 @@
 
             <div class="col-md-8 col-xl-9">
               <div class="row gap-y">
-
+                @foreach($posts as $post)
                 <div class="col-md-6">
                   <div class="card border hover-shadow-6 mb-6 d-block">
-                    <a href="#"><img class="card-img-top" src="../assets/img/thumb/1.jpg" alt="Card image cap"></a>
+                    <a href="#"><img class="card-img-top" src="storage/{{ $post->image }}" alt="Card image cap"></a>
                     <div class="p-6 text-center">
-                      <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="#">News</a></p>
-                      <h5 class="mb-0"><a class="text-dark" href="#">We relocated our office to a new designed garage</a></h5>
+                      <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="#">
+                        {{ $post->category->name }}
+                      </a></p>
+                      <h5 class="mb-0"><a class="text-dark" href="#">
+                        {{ $post->title }}
+                      </a></h5>
                     </div>
                   </div>
                 </div>
-
-
-                <div class="col-md-6">
-                  <div class="card border hover-shadow-6 mb-6 d-block">
-                    <a href="#"><img class="card-img-top" src="../assets/img/thumb/2.jpg" alt="Card image cap"></a>
-                    <div class="p-6 text-center">
-                      <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="#">Marketing</a></p>
-                      <h5 class="mb-0"><a class="text-dark" href="#">Top 5 brilliant content marketing strategies</a></h5>
-                    </div>
-                  </div>
-                </div>
-
-
-                <div class="col-md-6">
-                  <div class="card border hover-shadow-6 mb-6 d-block">
-                    <a href="#"><img class="card-img-top" src="../assets/img/thumb/3.jpg" alt="Card image cap"></a>
-                    <div class="p-6 text-center">
-                      <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="#">Design</a></p>
-                      <h5 class="mb-0"><a class="text-dark" href="#">Best practices for minimalist design with example</a></h5>
-                    </div>
-                  </div>
-                </div>
-
-
-                <div class="col-md-6">
-                  <div class="card border hover-shadow-6 mb-6 d-block">
-                    <a href="#"><img class="card-img-top" src="../assets/img/thumb/4.jpg" alt="Card image cap"></a>
-                    <div class="p-6 text-center">
-                      <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="#">Hiring</a></p>
-                      <h5 class="mb-0"><a class="text-dark" href="#">Congratulate and thank to Maryam for joining our team</a></h5>
-                    </div>
-                  </div>
-                </div>
-
-
-                <div class="col-md-6">
-                  <div class="card border hover-shadow-6 mb-6 d-block">
-                    <a href="#"><img class="card-img-top" src="../assets/img/thumb/5.jpg" alt="Card image cap"></a>
-                    <div class="p-6 text-center">
-                      <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="#">Product</a></p>
-                      <h5 class="mb-0"><a class="text-dark" href="#">New published books to read by a product designer</a></h5>
-                    </div>
-                  </div>
-                </div>
-
-
-                <div class="col-md-6">
-                  <div class="card border hover-shadow-6 mb-6 d-block">
-                    <a href="#"><img class="card-img-top" src="../assets/img/thumb/6.jpg" alt="Card image cap"></a>
-                    <div class="p-6 text-center">
-                      <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="#">Management</a></p>
-                      <h5 class="mb-0"><a class="text-dark" href="#">This is why it's time to ditch dress codes at work</a></h5>
-                    </div>
-                  </div>
-                </div>
-
+                @endforeach
               </div>
 
 
@@ -444,19 +393,18 @@
 
                 <h6 class="sidebar-title">Categories</h6>
                 <div class="row link-color-default fs-14 lh-24">
-                  <div class="col-6"><a href="#">News</a></div>
-                  <div class="col-6"><a href="#">Updates</a></div>
-                  <div class="col-6"><a href="#">Design</a></div>
-                  <div class="col-6"><a href="#">Marketing</a></div>
-                  <div class="col-6"><a href="#">Partnership</a></div>
-                  <div class="col-6"><a href="#">Product</a></div>
-                  <div class="col-6"><a href="#">Hiring</a></div>
-                  <div class="col-6"><a href="#">Offers</a></div>
+                  @foreach($categories as $category)
+                  <div class="col-6">
+                    <a href="#">
+                      {{ $category->name }}
+                    </a>
+                  </div>
+                  @endforeach
                 </div>
 
                 <hr>
 
-                <h6 class="sidebar-title">Top posts</h6>
+                <!-- <h6 class="sidebar-title">Top posts</h6>
                 <a class="media text-default align-items-center mb-5" href="blog-single.html">
                   <img class="rounded w-65px mr-4" src="../assets/img/thumb/4.jpg">
                   <p class="media-body small-2 lh-4 mb-0">Thank to Maryam for joining our team</p>
@@ -477,27 +425,18 @@
                   <p class="media-body small-2 lh-4 mb-0">Top 5 brilliant content marketing strategies</p>
                 </a>
 
-                <hr>
+                <hr> -->
 
                 <h6 class="sidebar-title">Tags</h6>
                 <div class="gap-multiline-items-1">
-                  <a class="badge badge-secondary" href="#">Record</a>
-                  <a class="badge badge-secondary" href="#">Progress</a>
-                  <a class="badge badge-secondary" href="#">Customers</a>
-                  <a class="badge badge-secondary" href="#">Freebie</a>
-                  <a class="badge badge-secondary" href="#">Offer</a>
-                  <a class="badge badge-secondary" href="#">Screenshot</a>
-                  <a class="badge badge-secondary" href="#">Milestone</a>
-                  <a class="badge badge-secondary" href="#">Version</a>
-                  <a class="badge badge-secondary" href="#">Design</a>
-                  <a class="badge badge-secondary" href="#">Customers</a>
-                  <a class="badge badge-secondary" href="#">Job</a>
+                  @foreach($tags as $tag)
+                    <a class="badge badge-secondary" href="#">
+                      {{ $tag->name }}
+                    </a>
+                  @endforeach
                 </div>
 
                 <hr>
-
-                <h6 class="sidebar-title">About</h6>
-                <p class="small-3">TheSaaS is a responsive, professional, and multipurpose SaaS, Software, Startup and WebApp landing template powered by Bootstrap 4. TheSaaS is a powerful and super flexible tool for any kind of landing pages.</p>
 
 
               </div>
@@ -515,7 +454,7 @@
         <div class="row gap-y align-items-center">
 
           <div class="col-6 col-lg-3">
-            <a href="../index.html"><img src="../assets/img/logo-dark.png" alt="logo"></a>
+            <a href="/"><img src="{{ asset('img/logo-dark.png') }}" alt="logo"></a>
           </div>
 
           <div class="col-6 col-lg-3 text-right order-lg-last">
@@ -527,15 +466,7 @@
             </div>
           </div>
 
-          <div class="col-lg-6">
-            <div class="nav nav-bold nav-uppercase nav-trim justify-content-lg-center">
-              <a class="nav-link" href="../uikit/index.html">Elements</a>
-              <a class="nav-link" href="../block/index.html">Blocks</a>
-              <a class="nav-link" href="../page/about-1.html">About</a>
-              <a class="nav-link" href="../blog/grid.html">Blog</a>
-              <a class="nav-link" href="../page/contact-1.html">Contact</a>
-            </div>
-          </div>
+
 
         </div>
       </div>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
+
+
 
 Auth::routes();
 

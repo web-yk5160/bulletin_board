008c6810075da486b1b9a7680dc4c0613a62b882
web-yk5160 <rubystudy0123@gmail.com>
2019-06-20 15:56:23 +0900

    Added search function

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@ Sass Blog
 
             <div class="col-md-8 col-xl-9">
               <div class="row gap-y">
-                @foreach($posts as $post)
+                @forelse($posts as $post)
                 <div class="col-md-6">
                   <div class="card border hover-shadow-6 mb-6 d-block">
                     <a href="{{ route('blog.show', $post->id) }}"><img class="card-img-top" src="storage/{{ $post->image }}" alt="Card image cap"></a>
@@ -46,12 +46,16 @@ Sass Blog
                     </div>
                   </div>
                 </div>
-                @endforeach
+                @empty
+                <p class="text-center">
+                  No result found for query <strong>{{ request()->query('search') }}</strong>
+                </p>
+                @endforelse
               </div>
 
 
 
-              {{ $posts->links() }}
+              {{ $posts->appends(['search' => request()->query('search')])->links() }}
             </div>
 
 
@@ -60,8 +64,8 @@ Sass Blog
               <div class="sidebar px-4 py-md-0">
 
                 <h6 class="sidebar-title">Search</h6>
-                <form class="input-group" target="#" method="GET">
-                  <input type="text" class="form-control" name="s" placeholder="Search">
+                <form class="input-group" aciton="{{ route('welcome') }}" method="GET">
+                  <input type="text" class="form-control" name="search" placeholder="Search" value="{{ request()->query('search') }}">
                   <div class="input-group-addon">
                     <span class="input-group-text"><i class="ti-search"></i></span>
                   </div>
